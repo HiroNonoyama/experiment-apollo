fac0af3f5e5f45b3c6d06379a99dd38719c3fda7
Hiro Nonoyama <h.nonoyama.117@gmail.com>
2023-11-29 23:13:07 +0900

    test subscription

--- index.ts
+++ index.ts
@@ -1,13 +1,29 @@
-import { schema } from './src/schema';
-import { WebSocketServer } from 'ws'
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import express from "express";
+import { useServer } from "graphql-ws/lib/use/ws";
+import { WebSocketServer } from "ws";
+import { schema } from "./src/schema";
 
-import {useServer} from 'graphql-ws/lib/use/ws'
+(async () => {
+  const apolloServer = new ApolloServer({ schema });
+  await apolloServer.start();
 
-const server = new WebSocketServer({
-  port: 1234,
-  path: '/graphql'
-})
+  const app = express();
+  app.use(
+    "/graphql",
+    express.json(),
+    expressMiddleware(apolloServer)
+  );
 
-useServer({ schema }, server)
+  const server = app.listen(1234, () => {
+    const wsServer = new WebSocketServer({
+      server,
+      path: "/graphql",
+    });
+    useServer({ schema }, wsServer);
+  });
 
-console.log('Listening on ws://localhost:1234')
+  console.log("Listening on http://localhost:1234/graphql");
+  console.log("Listening on ws://localhost:1234/ws");
+})();
--- src/schema.ts
+++ src/schema.ts
@@ -1,26 +1,61 @@
 import { GraphQLObjectType, GraphQLSchema, GraphQLString } from "graphql";
+import { RedisPubSub } from "graphql-redis-subscriptions";
+import { PubSubRedisOptions } from "graphql-redis-subscriptions/dist/redis-pubsub";
+
+const options: PubSubRedisOptions["connection"] = {
+  host: "redis",
+  retryStrategy: (times) => {
+    return Math.min(times * 50, 2000);
+  },
+};
+const pubSub = new RedisPubSub({ connection: options });
+
+const MessageType = new GraphQLObjectType({
+  name: "Message",
+  fields: {
+    id: { type: GraphQLString },
+    text: { type: GraphQLString },
+  },
+});
 
 export const schema = new GraphQLSchema({
   query: new GraphQLObjectType({
     name: "Query",
     fields: {
-      hello: {
-        type: GraphQLString,
-        resolve: () => "Hello from the Query!"
-      }
+      getMessage: {
+        type: MessageType,
+        resolve: async () => {
+          const message = { id: "1", text: "Hello world" };
+          pubSub.publish("MESSAGE_ADDED", { message });
+          return message;
+        },
+      },
+    },
+  }),
+  mutation: new GraphQLObjectType({
+    name: "Mutation",
+    fields: {
+      createMessage: {
+        type: MessageType,
+        args: { text: { type: GraphQLString } },
+        resolve: async (_, { text }) => {
+          const message = {
+            id: "2",
+            text,
+          };
+          await pubSub.publish("MESSAGE_ADDED", { message });
+          return message;
+        },
+      },
     },
   }),
   subscription: new GraphQLObjectType({
-    name: 'Subscription',
+    name: "Subscription",
     fields: {
-      greetings: {
-        type: GraphQLString,
-        subscribe: function* () {
-          for (let i = 0; i < 5; i++) {
-            yield { greetings: `Hello from the Subscription! ${i}` }
-          }
-        }
-      }
-    }
-  })
-})
+      message: {
+        type: MessageType,
+        subscribe: () => pubSub.asyncIterator("MESSAGE_ADDED"),
+      },
+    },
+  }),
+});
